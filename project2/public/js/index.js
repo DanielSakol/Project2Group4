36e5cb1f7b84c6811dc15b6d455343a816be314a
Danny Acne <dannyacne@gmail.com>
2019-05-11 13:03:59 -0500

    firebase auth and homepage

--- project2/public/js/index.js
+++ project2/public/js/index.js
@@ -1,12 +1,12 @@
 // Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+let $exampleText = $('#example-text');
+let $exampleDescription = $('#example-description');
+let $submitBtn = $('#submit');
+let $exampleList = $('#example-list');
 
 // The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
+let API = {
+  saveExample(example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -16,23 +16,23 @@ var API = {
       data: JSON.stringify(example)
     });
   },
-  getExamples: function() {
+  getExamples() {
     return $.ajax({
       url: "api/examples",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  deleteExample(id) {
     return $.ajax({
       url: "api/examples/" + id,
       type: "DELETE"
     });
-  }
+  },
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
+let refreshExamples = function () {
+  API.getExamples().then((data) => {
     var $examples = data.map(function(example) {
       var $a = $("<a>")
         .text(example.text)
@@ -61,39 +61,39 @@ var refreshExamples = function() {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+let handleFormSubmit = function (event) {
   event.preventDefault();
 
-  var example = {
+  let example = {
     text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
+    description: $exampleDescription.val().trim(),
   };
 
   if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
+    alert('You must enter an example text and description!');
     return;
   }
 
-  API.saveExample(example).then(function() {
+  API.saveExample(example).then(() => {
     refreshExamples();
   });
 
-  $exampleText.val("");
-  $exampleDescription.val("");
+  $exampleText.val('');
+  $exampleDescription.val('');
 };
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
+let handleDeleteBtnClick = function () {
+  let idToDelete = $(this)
     .parent()
-    .attr("data-id");
+    .attr('data-id');
 
-  API.deleteExample(idToDelete).then(function() {
+  API.deleteExample(idToDelete).then(() => {
     refreshExamples();
   });
 };
 
 // Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+$submitBtn.on('click', handleFormSubmit);
+$exampleList.on('click', '.delete', handleDeleteBtnClick);
