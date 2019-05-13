e4671cdbdb582c72f9f52f1c16df2a5380e77bd1
YinshengGuo <yinshengguo@gmail.com>
2019-05-12 20:13:57 -0500

    usda query with upc and ndbno

--- project2/routes/apiRoutes.js
+++ project2/routes/apiRoutes.js
@@ -1,24 +1,88 @@
 var db = require("../models");
+const axios = require("axios");
 
-module.exports = function(app) {
+const genUpcQueryUrl = function (upcCode) {
+  const baseUrl = 'https://api.nal.usda.gov/ndb/search/?';
+  const params = {
+    format: 'json',
+    q: upcCode,
+    api_key: process.env.usda_key
+  };
+  // QueryUrl = 'https://api.nal.usda.gov/ndb/search/?format=json&q=070038630678&api_key=0RYf48D8ckcFA864kSW8vl49MtNxv2o99FhLHP01';
+  queryUrl = baseUrl + Object.keys(params).map(k => k + '=' + params[k]).join('&');
+  return queryUrl;
+}
+
+const genNdbnoQueryUrl = function (ndbno) {
+  // const baseUrl = 'https://api.nal.usda.gov/ndb/search/?';
+  const baseUrl = 'https://api.nal.usda.gov/ndb/reports/?';
+  const params = {
+    format: 'json',
+    type:'s', // [b]asic, [f]ull, [s]tats
+    ndbno: [ndbno], // a list of up to 25 nbd numbers
+    api_key: process.env.usda_key
+  };
+  // QueryUrl = 'https://api.nal.usda.gov/ndb/search/?format=json&ndbno=45346780&api_key=0RYf48D8ckcFA864kSW8vl49MtNxv2o99FhLHP01';
+  queryUrl = baseUrl + Object.keys(params).map(k => k + '=' + params[k]).join('&');
+  return queryUrl;
+}
+
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/examples", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
+  });
+
+  app.get("/api/dev", function (req, res) {
+    // calling res.send() here is problematic for the other res.send() call later
+    // res.send("custom api called");
+    // console.log("custom api called");
+    const upc = '070038630678';//'096619756803'
+    const upcQueryUrl = genUpcQueryUrl(upc);
+    console.log('UPC query url:\n', upcQueryUrl);
+    axios
+      .get(upcQueryUrl)
+      .then(function (response) {
+        // console.log(response);
+        // res.send(response.data);
+
+        const itemLst = response.data.list.item;
+        const ndbLst = itemLst.map(v => v.ndbno);
+        console.log('item list matching upc code: ', itemLst);
+        console.log('extracted ndbno: ', ndbLst);
+
+        if (ndbLst) {
+          const ndbQueryUrl = genNdbnoQueryUrl(ndbLst[0]);
+          console.log('ndbno query url:\n', ndbQueryUrl);
+          axios.get(ndbQueryUrl)
+            .then(response => {
+              // console.log(response.data);
+
+              res.send(response.data);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   });
 };
--- project2/server.js
+++ project2/server.js
@@ -37,41 +37,41 @@ if (process.env.NODE_ENV === 'test') {
 db.sequelize.sync(syncOptions).then(() => {
   app.listen(PORT, () => {
     console.log(
-      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+      '==> Listening on port %s. Visit http://localhost:%s/ in your browser.',
       PORT,
       PORT,
     );
   });
 });
 
-const firebase = require('firebase/app');
+// const firebase = require('firebase/app');
 
-require('firebase/auth');
+// require('firebase/auth');
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAYVGGhcwOrO97G3CpRsgEWvr5QqUsKpvA',
-  authDomain: 'group4proj2.firebaseapp.com',
-  databaseURL: 'https://group4proj2.firebaseio.com',
-  projectId: 'group4proj2',
-  storageBucket: 'group4proj2.appspot.com',
-  messagingSenderId: '839976170219',
-  appId: '1:839976170219:web:ccc53f18cdabca1c',
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAYVGGhcwOrO97G3CpRsgEWvr5QqUsKpvA',
+//   authDomain: 'group4proj2.firebaseapp.com',
+//   databaseURL: 'https://group4proj2.firebaseio.com',
+//   projectId: 'group4proj2',
+//   storageBucket: 'group4proj2.appspot.com',
+//   messagingSenderId: '839976170219',
+//   appId: '1:839976170219:web:ccc53f18cdabca1c',
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-firebase.auth().signInWithEmailAndPassword('daniel.sakol@yahoo.com', '123456')
-  .then((user) => {
-    if (user) {
-      console.log(user);
-    }
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    let errorCode = error.code;
-    let errorMessage = error.message;
+// firebase.auth().signInWithEmailAndPassword('daniel.sakol@yahoo.com', '123456')
+//   .then((user) => {
+//     if (user) {
+//       console.log(user);
+//     }
+//   })
+//   .catch((error) => {
+//     // Handle Errors here.
+//     let errorCode = error.code;
+//     let errorMessage = error.message;
 
-    console.log(errorCode, errorMessage);
-  });
+//     console.log(errorCode, errorMessage);
+//   });
 
 module.exports = app;
