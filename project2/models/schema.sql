01e383e9a653ae9486ba6df4f0cdb83bc5a9c59e
Moh Chica <onibadesh@yahoo.com>
2019-05-13 19:46:29 -0500

    create table user products

--- project2/models/schema.sql
+++ project2/models/schema.sql
@@ -1,5 +1,15 @@
-DROP DATABASE IF EXISTS exampledb;
+/*DROP DATABASE IF EXISTS exampledb;
 CREATE DATABASE exampledb;
 
 DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE DATABASE testdb;*/
+
+CREATE TABLE user_products (
+id int(11) NOT NULL AUTO_INCREMENT,
+user_id int(11) NOT NULL,
+product_id int(11) NOT NULL,
+favorite tinyint(1) DEFAULT NULL,
+product_name varchar(100) DEFAULT NULL,
+product_url varchar(255) DEFAULT NULL,
+PRIMARY KEY (id)
+);
