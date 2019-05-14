f756da9bb5afec3ee5de6ab083b49c10f7fa604e
Turbo <toan.takahara@gmail.com>
2019-05-13 21:42:15 -0500

    product html page added

--- project2/routes/htmlRoutes.js
+++ project2/routes/htmlRoutes.js
@@ -25,6 +25,19 @@ module.exports = function (app) {
       });
   });
 
+  // Load product page
+  app.get("/product", function (req, res) {
+    db.Example.findAll({})
+      .then(function (dbExamples) {
+        // res.render("index", {
+        //   msg: "Welcome!",
+        //   examples: dbExamples
+        // });
+        res.render("product");
+      });
+  });
+  
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function (req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
@@ -33,6 +46,19 @@ module.exports = function (app) {
       });
     });
   });
+
+    // Load product page
+    app.get("/barcode", function (req, res) {
+      db.Example.findAll({})
+        .then(function (dbExamples) {
+          // res.render("index", {
+          //   msg: "Welcome!",
+          //   examples: dbExamples
+          // });
+          res.render("barcode");
+        });
+    });
+
 
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
