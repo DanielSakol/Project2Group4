a7bdccab8eaa7d5eedebb133b17cb81e80d7ee1d
Moh Chica <onibadesh@yahoo.com>
2019-05-13 19:51:47 -0500

    crteate table users

--- project2/models/schema.sql
+++ project2/models/schema.sql
@@ -4,12 +4,23 @@ CREATE DATABASE exampledb;
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;*/
 
+CREATE TABLE users (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    first_name varchar(100) DEFAULT NULL,
+    last_name varchar(100) DEFAULT NULL,
+    email varchar(75) DEFAULT NULL,
+    username varchar(100) DEFAULT NULL,
+    userpassword varchar(255) DEFAULT NULL,
+    PRIMARY KEY (id)
+)
+
+
 CREATE TABLE user_products (
-id int(11) NOT NULL AUTO_INCREMENT,
-user_id int(11) NOT NULL,
-product_id int(11) NOT NULL,
-favorite tinyint(1) DEFAULT NULL,
-product_name varchar(100) DEFAULT NULL,
-product_url varchar(255) DEFAULT NULL,
-PRIMARY KEY (id)
+    id int(11) NOT NULL AUTO_INCREMENT,
+    user_id int(11) NOT NULL,
+    product_id int(11) NOT NULL,
+    favorite tinyint(1) DEFAULT NULL,
+    product_name varchar(100) DEFAULT NULL,
+    product_url varchar(255) DEFAULT NULL,
+    PRIMARY KEY (id)
 );
