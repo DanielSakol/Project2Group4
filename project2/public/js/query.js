cc6ec18b1df0ca7bbd13c988c8b86576cd939d8a
Turbo <toan.takahara@gmail.com>
2019-05-14 22:49:26 -0500

    added generic profile page

--- project2/public/js/query.js
+++ project2/public/js/query.js
@@ -15,10 +15,17 @@ let searchBtnFn = function (event) {
         data: queryJSON
     }).then(response => {
         console.log(response);
-        // add code here to display response json on page
+        // empty out the product-name/ingredient 
+        // take response and append into product-name/ingredient
+        $('#product-name').empty(); $('#product-name').append(JSON.stringify(response.foods[0].food.desc.name))
+        $('#product-ingredient').empty(); $('#product-ingredient').append(JSON.stringify(response.foods[0].food.ing.desc))
     }).catch(error => {
         console.log(error);
     })
 }
 
 $("#searchBtn").on('click', searchBtnFn);
+
+
+
+$('div#1').empty(); $('div#1').append('your code here');
